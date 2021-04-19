909415b3837ddf354d204b2330a2ebe9e0387155
r3nanp <renanmol87@gmail.com>
2021-04-19 09:33:56 -0300

    feat: create seo component

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -10,6 +10,29 @@ export function SEO({ title }: SEOProps): ReactElement {
     <>
       <Head>
         <title>{title}</title>
+        <meta name="keywords" content="podcast, audio" />
+        <meta name="description" content="The best podcast application." />
+
+        <meta property="og:site_name" content="Podcastr" />
+        <meta property="og:title" content="Podcastr" />
+        <meta
+          property="og:description"
+          content="The best podcast application."
+        />
+        <meta property="og:image" content="/thumb.svg" />
+        <meta property="og:image:type" content="image/svg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+
+        <meta name="twitter:image" content="/thumb.svg" />
+        <meta name="twiiter:image:alt" content="Thumbnail" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Podcastr" />
+        <meta
+          name="twitter:description"
+          content="The best podcast application."
+        />
+        <meta name="twiiter:create" content="r3nanp" />
       </Head>
     </>
   )
