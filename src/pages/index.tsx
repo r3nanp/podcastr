d9d73df7c6c19e55cf86a58d428262d8ef535f35
r3nanp <renanmol87@gmail.com>
2021-04-20 11:27:54 -0300

    feat(pages): add api request and create a static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,42 @@
 import { ReactElement } from 'react'
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import { SEO } from '@components/SEO'
 
-export default function Home(): ReactElement {
+interface EpisodeProps {
+  id: string
+  title: string
+}
+
+interface HomeProps {
+  episodes: EpisodeProps[]
+}
+
+export default function Home({ episodes }: HomeProps): ReactElement {
   return (
     <>
       <SEO title="Podcastr | Home" />
-
-      <main>
-        <h2>Hello world!</h2>
-      </main>
+      <section>
+        {episodes.map(episode => (
+          <div key={episode.id} style={{ margin: '1rem' }}>
+            <Link href={`/episodes/${episode.id}`}>
+              <a>{episode.title}</a>
+            </Link>
+          </div>
+        ))}
+      </section>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const request = await fetch('http://localhost:3333/episodes')
+  const data = await request.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8
+  }
 }
