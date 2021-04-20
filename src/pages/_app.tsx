a517210463dcb10bd61598f89017acfed17b0cd3
r3nanp <renanmol87@gmail.com>
2021-04-20 11:26:58 -0300

    feat: add components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import { ReactElement } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
+
 import { theme } from '@styles/theme'
+import { GlobalStyles } from '@styles/global'
+import { Wrapper } from '@styles/app'
+import { Header } from '@components/Header'
+import { Player } from '@components/Player'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <GlobalStyles />
+      <Wrapper>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+
+        <Player />
+      </Wrapper>
     </ThemeProvider>
   )
 }
