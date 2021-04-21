813bf92d235bc599523bdc88f530aaf03311498c
r3nanp <renanmol87@gmail.com>
2021-04-21 12:15:27 -0300

    styles: create page styles

---
+++ src/styles/pages/episode.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 45rem;
+  padding: 3rem 2rem;
+  margin: 0 auto;
+
+  height: calc(100vh - 6.5rem);
+  overflow-y: scroll;
+`
+
+export const ThumbnailContainer = styled.div`
+  position: relative;
+
+  img {
+    border-radius: 1rem;
+  }
+
+  button {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 0.75rem;
+
+    border: 0;
+    position: absolute;
+    z-index: 5;
+
+    font-size: 0;
+    transition: filter 0.3s;
+
+    &:first-child {
+      left: 0;
+      top: 50%;
+      background: var(--purple-500);
+      transform: translate(-50%, -50%);
+    }
+
+    &:last-child {
+      right: 0;
+      top: 50%;
+      background: var(--green-500);
+      transform: translate(50%, -50%);
+    }
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`
+
+export const Header = styled.header`
+  padding-bottom: 1rem;
+  border-bottom: 1px solid var(--gray-100);
+
+  h1 {
+    margin-top: 2rem;
+    margin-bottom: 1.5rem;
+  }
+
+  span {
+    display: inline-block;
+    font-size: 0.875rem;
+
+    & + span {
+      margin-left: 1rem;
+      padding-left: 1rem;
+      position: relative;
+
+      &::before {
+        content: '';
+        width: 4px;
+        height: 4px;
+        border-radius: 2px;
+        background: #ddd;
+        position: absolute;
+        left: 0;
+        top: 50%;
+
+        transform: translate(-50%, -50%);
+      }
+    }
+  }
+`
+
+export const Description = styled.div`
+  text-align: justify;
+  margin-top: 2rem;
+  line-height: 1.675rem;
+  color: var(--gray-800);
+
+  p {
+    margin: 1.5rem 0;
+  }
+`
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,175 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  padding: 0 4rem;
+  height: calc(100vh - 6.5rem);
+  overflow-y: scroll;
+
+  h2 {
+    margin-top: 3rem;
+    margin-bottom: 1.5rem;
+  }
+`
+
+export const LatestEpisodes = styled.section`
+  ul {
+    list-style: none;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1.5rem;
+  }
+`
+
+export const EpisodeList = styled.li`
+  background: var(--white);
+  border: 1px solid var(--gray-100);
+  padding: 1.25rem;
+  border-radius: 1.5rem;
+  position: relative;
+
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 6rem;
+    height: 6rem;
+    border-radius: 1rem;
+  }
+
+  button {
+    font-size: 0;
+    position: absolute;
+    right: 2rem;
+    bottom: 2rem;
+
+    width: 2.5rem;
+    height: 2.5rem;
+    background: var(--white);
+    border: 1px solid var(--gray-100);
+    border-radius: 0.675rem;
+    transition: filter 0.3s;
+
+    img {
+      width: 1.5rem;
+      height: 1.5rem;
+    }
+
+    &:hover {
+      filter: brightness(0.95);
+    }
+  }
+`
+
+export const AllEpisodes = styled.section`
+  padding-bottom: 2rem;
+
+  table {
+    width: 100%;
+
+    th,
+    td {
+      padding: 0.75rem 1rem;
+      border-bottom: 1px solid var(--gray-100);
+    }
+
+    th {
+      color: var(--gray-200);
+      text-transform: uppercase;
+      font: 500 0.75rem Lexend, sans-serif;
+      text-align: left;
+    }
+
+    td {
+      font-size: 0.875rem;
+
+      .img-thumb {
+        width: 2.5rem;
+        height: 2.5rem;
+        border-radius: 0.5rem;
+      }
+
+      a {
+        color: var(--gray-600);
+        font-family: Lexend, sans-serif;
+        font-weight: 600;
+        line-height: 1.4rem;
+        font-size: 1rem;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+
+      button {
+        font-size: 0;
+
+        width: 2rem;
+        height: 2rem;
+        background: var(--white);
+        border: 1px solid var(--gray-100);
+        border-radius: 0.675rem;
+        transition: filter 0.3s;
+
+        img {
+          width: 1.25rem;
+          height: 1.25rem;
+        }
+
+        &:hover {
+          filter: brightness(0.95);
+        }
+      }
+    }
+  }
+`
+
+export const EpisodeDetails = styled.div`
+  flex: 1;
+  margin-left: 1rem;
+
+  a {
+    display: block;
+    color: var(--gray-800);
+    font-family: Lexend, sans-serif;
+    font-weight: 600;
+    line-height: 1.4rem;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  p {
+    font-size: 0.875rem;
+    margin-top: 0.5rem;
+    max-width: 70%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  span {
+    display: inline-block;
+    margin-top: 0.5rem;
+    font-size: 0.875rem;
+
+    &:last-child {
+      margin-left: 0.5rem;
+      padding-left: 0.5rem;
+      position: relative;
+
+      &::before {
+        content: '';
+        width: 4px;
+        height: 4px;
+        border-radius: 2px;
+        background: #ddd;
+        position: absolute;
+        left: 0;
+        top: 50%;
+
+        transform: translate(-50%, -50%); // não é feitiçaria, é tecnologia.
+      }
+    }
+  }
+`
