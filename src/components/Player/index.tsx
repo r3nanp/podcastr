3dd8c76b7a2f7d13e16219713711fdaf5b1daf2e
r3nanp <renanmol87@gmail.com>
2021-04-22 14:31:03 -0300

    feat: create player functionality

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,8 +1,36 @@
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useRef } from 'react'
+import { usePlayer } from '@hooks/usePlayer'
+import Image from 'next/image'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 
 import * as S from './styles'
 
 export function Player(): ReactElement {
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const {
+    episodeList,
+    currentEpisodeIndex,
+    isPlaying,
+    togglePlay,
+    setPlayingState
+  } = usePlayer()
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      return
+    }
+
+    if (isPlaying) {
+      audioRef.current.play()
+    } else {
+      audioRef.current.pause()
+    }
+  }, [isPlaying])
+
+  const episode = episodeList[currentEpisodeIndex]
+
   return (
     <S.Container>
       <S.Header>
@@ -10,38 +38,80 @@ export function Player(): ReactElement {
         <strong>Tocando agora</strong>
       </S.Header>
 
-      <S.EmptyPlayer>
-        <strong>Selecione um podcast para ouvir.</strong>
-      </S.EmptyPlayer>
+      {episode ? (
+        <S.CurrentEpisode>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            alt={episode.title}
+            objectFit="cover"
+          />
 
-      <S.Footer className="empty">
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </S.CurrentEpisode>
+      ) : (
+        <S.EmptyPlayer>
+          <strong>Selecione um podcast para ouvir.</strong>
+        </S.EmptyPlayer>
+      )}
+
+      <S.Footer className={!episode ? 'empty' : 'playing'}>
         <S.Progress>
           <span>00:00</span>
 
           <div className="slider">
-            <div className="emptySlider" />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className="emptySlider" />
+            )}
           </div>
           <span>00:00</span>
         </S.Progress>
 
+        {episode && (
+          <audio
+            ref={audioRef}
+            src={episode.url}
+            autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
+
         <S.Buttons>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
 
-          <button type="button" className="playButton">
-            <img src="/play.svg" alt="Tocar" />
+          <button
+            type="button"
+            className="playButton"
+            onClick={togglePlay}
+            disabled={!episode}
+          >
+            {isPlaying ? (
+              <img src="/pause.svg" alt="Pausar" />
+            ) : (
+              <img src="/play.svg" alt="Tocar" />
+            )}
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
 
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </S.Buttons>
