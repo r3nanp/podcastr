ac87113f49d9d3aa46928829f8c3b3757de0bc2b
r3nanp <renanmol87@gmail.com>
2021-04-23 16:08:15 -0300

    feat(contexts): create theme context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,52 @@
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  ReactElement,
+  useState
+} from 'react'
+import { ThemeProvider as ThemeStyledProvider } from 'styled-components'
+
+import { GlobalStyles } from '@styles/global'
+import { themes, ThemeName } from '@styles/theme'
+
+export interface ThemeContextData {
+  switchTheme: () => void
+  theme: ThemeName
+}
+
+interface ThemeProps {
+  children: ReactNode
+}
+
+export const ThemeContext = createContext({} as ThemeContextData)
+
+export function ThemeProvider({ children }: ThemeProps): ReactElement {
+  const [theme, setTheme] = useState<ThemeName>('light')
+  const currentTheme = themes[theme]
+
+  function switchTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    const themeStoraged = localStorage.getItem('@MoveIt:theme')
+
+    if (themeStoraged) {
+      setTheme(JSON.parse(themeStoraged))
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('@MoveIt:theme', JSON.stringify(theme))
+  }, [theme])
+
+  return (
+    <ThemeContext.Provider value={{ switchTheme, theme }}>
+      <ThemeStyledProvider theme={currentTheme}>
+        <GlobalStyles />
+        {children}
+      </ThemeStyledProvider>
+    </ThemeContext.Provider>
+  )
+}
