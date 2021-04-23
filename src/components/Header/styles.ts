2e3c817a13ed4920455dfd1ca335d03cc8e78270
r3nanp <renanmol87@gmail.com>
2021-04-23 16:10:47 -0300

    feat: add theme switcher

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,30 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { BiMoon, BiSun } from 'react-icons/bi'
+
+const iconsCss = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 2.25rem;
+  height: 2.25rem;
+  fill: var(--purple-500);
+  margin: 0 1rem;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`
+
+export const LightIcon = styled(BiMoon)`
+  ${iconsCss}
+`
+
+export const DarkIcon = styled(BiSun)`
+  ${iconsCss}
+`
 
 export const Container = styled.header`
   background: var(--white);
