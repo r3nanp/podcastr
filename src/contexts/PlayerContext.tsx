2f4f2bc67f194779fc5697edbd40a0b2ade37163
r3nanp <renanmol87@gmail.com>
2021-04-23 14:59:48 -0300

    feat(player context): create new functionalities

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -11,11 +11,21 @@ interface Episode {
 
 export interface PlayerContextData {
   isPlaying: boolean
+  isLooping: boolean
+  isShuffling: boolean
+  hasNext: boolean
+  hasPrevious: boolean
   episodeList: Episode[]
   currentEpisodeIndex: number
   play: (episode: Episode) => void
   setPlayingState: (state: boolean) => void
+  playList: (episodes: Episode[], index: number) => void
+  clearPlayerState: () => void
   togglePlay: () => void
+  toggleLoop: () => void
+  toggleShuffle: () => void
+  playNext: () => void
+  playPrev: () => void
 }
 
 interface PlayerProviderProps {
@@ -29,6 +39,8 @@ export function PlayerProvider({
 }: PlayerProviderProps): ReactElement {
   const [episodeList, setEpisodeList] = useState([])
   const [isPlaying, setIsPlaying] = useState(false)
+  const [isLooping, setIsLooping] = useState(false)
+  const [isShuffling, setIsShuffling] = useState(false)
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
 
   function play(episode: Episode): void {
@@ -37,22 +49,72 @@ export function PlayerProvider({
     setIsPlaying(true)
   }
 
+  function playList(episodes: Episode[], index: number): void {
+    setEpisodeList(episodes)
+    setCurrentEpisodeIndex(index)
+    setIsPlaying(true)
+  }
+
   function togglePlay(): void {
     setIsPlaying(!isPlaying)
+  }
+
+  function toggleLoop(): void {
+    setIsLooping(!isLooping)
+  }
+
+  function toggleShuffle(): void {
+    setIsShuffling(!isShuffling)
   }
 
   function setPlayingState(state: boolean): void {
     setIsPlaying(state)
   }
 
+  function clearPlayerState(): void {
+    setEpisodeList([])
+    setCurrentEpisodeIndex(0)
+  }
+
+  const hasPrevious = currentEpisodeIndex > 0
+  const hasNext = isShuffling || currentEpisodeIndex + 1 < episodeList.length
+
+  function playNext() {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(
+        Math.random() * episodeList.length
+      )
+
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+    } else if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1)
+    }
+  }
+
+  function playPrev() {
+    if (hasPrevious) {
+      setCurrentEpisodeIndex(currentEpisodeIndex - 1)
+    }
+  }
+
   return (
     <PlayerContext.Provider
       value={{
         play,
+        clearPlayerState,
+        playNext,
+        playPrev,
+        isShuffling,
+        toggleShuffle,
+        toggleLoop,
+        playList,
         episodeList,
         currentEpisodeIndex,
         isPlaying,
         togglePlay,
+        isLooping,
+        hasNext,
+        hasPrevious,
         setPlayingState
       }}
     >
