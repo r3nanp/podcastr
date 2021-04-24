7c2474d1f7aea8ec23a43f0717c3de4e9b1670fc
r3nanp <renanmol87@gmail.com>
2021-04-24 18:59:24 -0300

    chore: add manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,10 +41,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#ffffff" />
           <link rel="shortcut icon" href="/favicon.png" />
-
+          <link rel="manifest" href="/manifest.json" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
 
           <link
