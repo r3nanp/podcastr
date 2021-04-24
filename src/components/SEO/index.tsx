7f3fba726e1fd427307eb9a62d0b0efc4512cb09
r3nanp <renanmol87@gmail.com>
2021-04-24 18:59:02 -0300

    feat: add meta tags to SEO component

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -10,21 +10,29 @@ export function SEO({ title }: SEOProps): ReactElement {
     <>
       <Head>
         <title>{title}</title>
+        <meta name="application-name" content="Podcastr" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Podcastr" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
         <meta name="keywords" content="podcast, audio" />
         <meta name="description" content="The best podcast application." />
 
+        <meta property="og:url" content="https://podcastr.r3nanp.vercel.app" />
         <meta property="og:site_name" content="Podcastr" />
         <meta property="og:title" content="Podcastr" />
         <meta
           property="og:description"
           content="The best podcast application."
         />
-        <meta property="og:image" content="/thumb.svg" />
+        <meta property="og:image" content="/logo.svg" />
         <meta property="og:image:type" content="image/svg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
 
-        <meta name="twitter:image" content="/thumb.svg" />
+        <meta name="twitter:image" content="/logo.svg" />
         <meta name="twiiter:image:alt" content="Thumbnail" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Podcastr" />
