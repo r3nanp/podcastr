7f30519d5e15dc0ff5e19d7486cb3ca6f77c7298
r3nanp <renanmol87@gmail.com>
2021-04-28 20:02:41 -0300

    test: refactor

--- src/components/Header/test.spec.tsx
+++ src/components/Header/test.spec.tsx
@@ -1,14 +1,18 @@
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import 'jest-styled-components'
 
-import { Header } from './index'
-
-const screen = render(<Header />)
-
-describe('Header', () => {
+describe('<Header />', () => {
   it('should render the image inside the header component', () => {
-    const img = screen.getByAltText(/podcastr/i).parentElement
+    const img = screen.getByRole('img', {
+      name: /podcastr/i
+    })
 
     expect(img).toBeInTheDocument()
+  })
+
+  it('should get the text inside the header component', () => {
+    const title = screen.getByText(/o melhor para você ouvir, sempre\./i)
+
+    expect(title).toBeInTheDocument()
   })
 })
